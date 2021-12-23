e8f153c353340b9be83d586652b361dfa46fdafa
Dzevad Trokic <d.trokic@cp-pro.de>
2021-12-23 14:44:08 +0100

    Projektdateien hinzufügen.

---
+++ SiebDesErstosthenes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SiebDesErstosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie die obere Grenze ein: ");
+            int UpperBound = int.Parse(Console.ReadLine()); // Zahl, bis z der gesucht werden soll, eingeben
+            bool[] Tagged = new bool[UpperBound+1]; // Array zur Speicherung ob die zum Index passende Zahl gestrichen wurde
+
+            // Array mit false initialisieren
+            for (int i = 0; i < Tagged.Length; i++)
+            {
+                Tagged[i] = false;
+            }
+
+            // alle Zahlen bis zur Wutzel der oberen zahl durchlaufen
+            for (int i = 2; i < Math.Ceiling(Math.Sqrt(UpperBound)); i++)
+            {
+                if (!Tagged[i])
+                {
+                    int j = i;
+
+            // alle Vielfachen der Zahl i, angefangen von ihrer Quadratzahl, durchstreichen
+            while (j * i <= UpperBound)
+                {
+                Tagged[j * i] = true;
+                j++;
+                }
+            }
+            }
+            for (int i = 0; i < Tagged.Length; i++)
+            {
+                if (!Tagged[i])
+                    Console.WriteLine(i.ToString()+ "");
+                {
+
+                }
+            }
+        }
+        
+    }
+}
