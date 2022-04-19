d5ed0eda1ae0296784b37dbfddf702c633eef573
Dzevad Trokic <d.trokic@cp-pro.de>
2022-04-19 14:34:43 +0200

    Kommentare entfert und aufegräumt

--- SiebDesErstosthenes/Program.cs
+++ SiebDesErstosthenes/Program.cs
@@ -7,20 +7,18 @@ namespace SiebDesErstosthenes
         static void Main(string[] args)
         {
             Console.WriteLine("Geben Sie die obere Grenze ein: ");
-            int UpperBound = int.Parse(Console.ReadLine()); // Zahl, bis z der gesucht werden soll, eingeben
-            bool[] Tagged = new bool[UpperBound + 1]; // Array zur Speicherung ob die zum Index passende Zahl gestrichen wurde
+            int UpperBound = int.Parse(Console.ReadLine());
+            bool[] Tagged = new bool[UpperBound + 1];
 
            
             ArrayFalsInitialsiert(Tagged);
 
-            // alle Zahlen bis zur Wutzel der oberen zahl durchlaufen
             for (int i = 2; i < Math.Ceiling(Math.Sqrt(UpperBound)); i++)
             {
                 if (!Tagged[i])
                 {
                     int j = i;
 
-                    // alle Vielfachen der Zahl i, angefangen von ihrer Quadratzahl, durchstreichen
                     while (j * i <= UpperBound)
                     {
                         Tagged[j * i] = true;
@@ -32,9 +30,6 @@ namespace SiebDesErstosthenes
             {
                 if (!Tagged[i])
                     Console.WriteLine(i.ToString() + "");
-                {
-
-                }
             }
         }
 
